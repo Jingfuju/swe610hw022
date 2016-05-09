0e9952c260a14ca87f8b169021ec40d1557354a2
jeffju <jujingfunanshi@hotmail.com>
2016-05-09 22:40:52 +0000

    Assignment2-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  
+  
+  get "dogs", to: "dog#index"
+  get "dogs/new", to: "dog#new"
+  get "dogs/:id", to: "dog#show", as: :dog
+  post "dogs", to: "dog#create"
+  
+  
   get "users", to: "users#index"
   get "users/new", to: "users#new"
   get "users/:id", to: "users#show", as: :user
   post "users", to: "users#create"
 end
+
+
